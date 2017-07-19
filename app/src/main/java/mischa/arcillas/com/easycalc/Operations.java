edac1018a81f624b0c58618a6d5c73bcfec99955
Mischa Alexa Arcillas <shtlangarcillas@gmail.com>
2017-07-19 22:56:47 +0800

    Initial Project Structure

---
+++ app/src/main/java/mischa/arcillas/com/easycalc/Operations.java
@@ -0,0 +1,23 @@
+package mischa.arcillas.com.easycalc;
+
+/**
+ * Created by Mischa Arcillas on 19/07/2017.
+ */
+
+class Operations {
+    public double add(double num1, double num2) {
+        return num1 + num2;
+    }
+
+    public double sub(double num1, double num2) {
+        return num1 - num2;
+    }
+
+    public double multiply(double num1, double num2) {
+        return num1 * num2;
+    }
+
+    public double divide(double num1, double num2) {
+        return num1 / num2;
+    }
+}
